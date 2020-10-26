44e33b9ab2836b7312bf02a7e058fdff7336e49e
ChaosNyaruko <cabbageparadise@gmail.com>
2020-10-26 20:20:59 +0800

    19 删除链表的倒数第n个节点

---
+++ 19.删除链表的倒数第n个节点.go
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=golang
+ *
+ * [19] 删除链表的倒数第N个节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{0, head}
+	fast := dummy
+	slow := dummy
+	for i := 0; i <= n && fast != nil; i++ {
+		fast = fast.Next
+	}
+	for fast != nil {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	slow.Next = slow.Next.Next
+	return dummy.Next
+}
+
+// @lc code=end
+
