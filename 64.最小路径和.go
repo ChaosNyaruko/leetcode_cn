e7790366d7cf1b2d1f47b65394723b8233bdcf5b
ChaosNyaruko <cabbageparadise@gmail.com>
2020-11-21 17:18:56 +0800

    64.最小路径和

---
+++ 64.最小路径和.go
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode.cn id=64 lang=golang
+ *
+ * [64] 最小路径和
+ *
+ * https://leetcode-cn.com/problems/minimum-path-sum/description/
+ *
+ * algorithms
+ * Medium (67.68%)
+ * Likes:    719
+ * Dislikes: 0
+ * Total Accepted:    160.1K
+ * Total Submissions: 236.5K
+ * Testcase Example:  '[[1,3,1],[1,5,1],[4,2,1]]'
+ *
+ * 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ * 
+ * 说明：每次只能向下或者向右移动一步。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
+ * 输出：7
+ * 解释：因为路径 1→3→1→1→1 的总和最小。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：grid = [[1,2,3],[4,5,6]]
+ * 输出：12
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * m == grid.length
+ * n == grid[i].length
+ * 1 
+ * 0 
+ * 
+ * 
+ */
+
+// @lc code=start
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func minPathSum(grid [][]int) int {
+	m := len(grid)
+	if m == 0 {
+		return 0
+	}
+	n := len(grid[0])
+	if n == 0 {
+		return 0
+	}
+	dp := make([][]int, m)
+	for i := 0; i < m; i++ {
+		dp[i] = make([]int, n)
+	}
+	dp[0][0] = grid[0][0]
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i > 0 && j > 0 {
+				dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+			} else if j> 0 {
+				dp[i][j] = dp[i][j-1] + grid[i][j]
+			} else if i > 0{
+				dp[i][j] = dp[i-1][j] + grid[i][j]
+			}
+		}
+	}
+	return dp[m-1][n-1]
+
+}
+// @lc code=end
+
