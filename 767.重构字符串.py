7fe76f3d439005ae41455ad72544813fbb114b92
ChaosNyaruko <cabbageparadise@gmail.com>
2020-11-30 15:51:34 +0800

    767.重构字符串-基于计数的解法

--- 767.重构字符串.py
+++ 767.重构字符串.py
@@ -6,7 +6,7 @@
 
 # @lc code=start
 class Solution:
-    def reorganizeString(self, S: str) -> str:
+    def reorganizeString_1(self, S: str) -> str:
         if len(S) < 2:
             return S
         
@@ -36,5 +36,31 @@ class Solution:
             ans.append(queue[0][1])
         
         return "".join(ans)
-       
+
+    def reorganizeString(self, S: str) -> str:
+        if len(S) < 2:
+            return S
+        
+        length = len(S)
+        counts = collections.Counter(S)
+        maxCount = max(counts.items(), key=lambda x: x[1])[1]
+        if maxCount > (length + 1) // 2:
+            return ""
+        
+        reorganizeArray = [""] * length
+        evenIndex, oddIndex = 0, 1
+        halfLength = length // 2
+
+        for c, count in counts.items():
+            while count > 0 and count <= halfLength and oddIndex < length:
+                reorganizeArray[oddIndex] = c
+                count -= 1
+                oddIndex += 2
+            while count > 0:
+                reorganizeArray[evenIndex] = c
+                count -= 1
+                evenIndex += 2
+        
+        return "".join(reorganizeArray)
+
 # @lc code=end
