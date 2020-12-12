bb30d4f8b1f3a80afd421cd6c7201d995c26c551
ChaosNyaruko <cabbageparadise@gmail.com>
2020-12-12 20:31:49 +0800

    198.打家劫舍.py

--- 198.打家劫舍.py
+++ 198.打家劫舍.py
@@ -49,5 +49,20 @@
 # @lc code=start
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # dp[i] 考虑偷到i(含)为止最大金额
+        # dp[i] = max(dp[i - 1],dp[i- 2] + nums[i]
+        # 不考虑空间优化
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+        dp = [0] * (n)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        return dp[n - 1]
+         
 # @lc code=end
 
