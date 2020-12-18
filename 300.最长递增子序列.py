3d884bf74065ab4bf2521a9b731c249bcc7cb91e
ChaosNyaruko <cabbageparadise@gmail.com>
2020-12-18 11:46:10 +0800

    300.最长递增子序列.py-tails nlog(n)解法

--- 300.最长递增子序列.py
+++ 300.最长递增子序列.py
@@ -6,7 +6,7 @@
 
 # @lc code=start
 class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
+    def lengthOfLIS_1(self, nums: List[int]) -> int:
         # normal n^2
         # dp[i] max inscreasing sequence ending at nums[i]
         if len(nums) == 0:
@@ -19,5 +19,29 @@ class Solution:
                     dp[i] = max(dp[i], dp[j] + 1)
             res = max(res, dp[i])
         return res
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # nlog(n)
+        # tails[i] means the minimum ending element among all LISs at length i+1
+        if len(nums) == 0:
+            return 0
+        tails = [nums[0]]
+        for i in range(1, len(nums)):
+            # find the smallest element bigger than (or equal to)nums[i] and replace it
+            # if nums[i] is smaller than (or equal to) all tails[i], tails[1]
+            if nums[i] > tails[-1]:
+                tails.append(nums[i])
+            else:
+                l, r = 0, len(tails) - 1
+                while l < r:
+                    m = l + (r - l) // 2
+                    if tails[m] < nums[i]:
+                        l = m + 1
+                    else:
+                        r = m
+                # print("tails", tails, "tails[l]", tails[l], "nums[i]", nums[i])
+                tails[l] = nums[i]
+        return len(tails)
+        # 爱你~
 # @lc code=end
 
