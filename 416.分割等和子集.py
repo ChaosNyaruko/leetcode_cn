98aaaab0d0defd33517e3c34c96b8502e9934051
ChaosNyaruko <cabbageparadise@gmail.com>
2020-12-23 11:16:30 +0800

    416.分割等和子集.py + debug

--- 416.分割等和子集.py
+++ 416.分割等和子集.py
@@ -18,13 +18,14 @@ class Solution:
         if nums[0] <= target:
             dp[0][nums[0]] = True 
         # print(dp)
-        # for i in range(0,len(nums)):
-        #     dp[i][0] = True
+        for i in range(0,len(nums)):
+            dp[i][0] = True
         for i in range(1, len(nums)):
             for j in range(0, target + 1):
                 dp[i][j] = dp[i-1][j] # do not use nums[i]
                 if j >= nums[i]:
                     dp[i][j] = dp[i][j] or dp[i-1][j - nums[i]]
+        # print(dp[2][11])
         return dp[len(nums) - 1][target]
 # @lc code=end
 
