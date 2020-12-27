b71525db89e66f66883f83094438c90df2733976
ChaosNyaruko <cabbageparadise@gmail.com>
2020-12-27 10:00:47 +0800

    647.回文子串.py

---
+++ 647.回文子串.py
@@ -0,0 +1,68 @@
+#
+# @lc app=leetcode.cn id=647 lang=python3
+#
+# [647] 回文子串
+#
+# https://leetcode-cn.com/problems/palindromic-substrings/description/
+#
+# algorithms
+# Medium (64.60%)
+# Likes:    459
+# Dislikes: 0
+# Total Accepted:    74K
+# Total Submissions: 114.6K
+# Testcase Example:  '"abc"'
+#
+# 给定一个字符串，你的任务是计算这个字符串中有多少个回文子串。
+# 
+# 具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 输入："abc"
+# 输出：3
+# 解释：三个回文子串: "a", "b", "c"
+# 
+# 
+# 示例 2：
+# 
+# 输入："aaa"
+# 输出：6
+# 解释：6个回文子串: "a", "a", "a", "aa", "aa", "aaa"
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 输入的字符串长度不会超过 1000 。
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        def extend(s, i, j) -> int:
+            if i > j or i >= len(s) or j >= len(s):
+                return 0
+            res = 0
+            while i >=0 and  j < len(s):
+                if s[i] == s[j]:
+                    res += 1
+                else: break
+                i -= 1
+                j += 1
+            return res
+        res = 0
+        for i in range(len(s)):
+            res += extend(s, i, i)
+            res += extend(s, i, i + 1)
+        return res
+
+
+        
+# @lc code=end
+
