2d2475c808c42a1bef46f2a630f42484c2e0cecc
ChaosNyaruko <cabbageparadise@gmail.com>
2021-01-01 01:42:24 +0800

    41.缺失的第一个正数.py - remove print

--- 41.缺失的第一个正数.py
+++ 41.缺失的第一个正数.py
@@ -50,7 +50,6 @@ class Solution:
         for i in range(len(nums)):
             while nums[i] >= 1 and nums[i] <= N and nums[i] != nums[nums[i] -1]:
                 nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
-                print("i=", i, nums)
         for i in range(N):
             if nums[i] != i + 1:
                 return i + 1
