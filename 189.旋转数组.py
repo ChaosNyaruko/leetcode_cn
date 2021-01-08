4e6b3d45d46068e82f60feaeaacd4cc0ce229d1c
ChaosNyaruko <cabbageparadise@gmail.com>
2021-01-08 08:48:44 +0800

    189.旋转数组.py-另一种写法

--- 189.旋转数组.py
+++ 189.旋转数组.py
@@ -17,9 +17,12 @@ class Solution:
                 nums[l], nums[r] = nums[r], nums[l]
                 l += 1
                 r -= 1
-        reverse(0, n - k - 1)
-        reverse(n - k, n - 1)
-        reverse(0, len(nums) - 1)
+        # reverse(0, n - k - 1)
+        # reverse(n - k, n - 1)
+        # reverse(0, len(nums) - 1)
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
             
             
 # @lc code=end
