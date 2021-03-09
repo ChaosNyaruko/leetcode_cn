132e22c5216c6e9387d1bdf846d002ec294d5d8c
ChaosNyaruko <cabbageparadise@gmail.com>
2021-03-09 09:09:10 +0800

    1047.删除字符串中所有的相邻重复项.py

---
+++ 1047.删除字符串中的所有相邻重复项.py
@@ -0,0 +1,9 @@
+class Solution:
+    def removeDuplicates(self, S: str) -> str:
+        stk = []
+        for c in S:
+            if stk and stk[-1] == c:
+                stk.pop()
+            else:
+                stk.append(c)
+        return ''.join(stk)
