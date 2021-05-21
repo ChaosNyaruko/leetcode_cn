3962caf0dfd51eed3292260b7cb4a6f3e5d90dc5
ChaosNyaruko <cabbageparadise@gmail.com>
2021-05-21 10:53:39 +0800

    1035.不相交的线.go

---
+++ 1035.不相交的线.go
@@ -0,0 +1,25 @@
+func maxUncrossedLines(nums1 []int, nums2 []int) int {
+	// dp[i][j] nums1前i个数与nums2前j个数的最大公共子序列长度
+	m, n := len(nums1), len(nums2)
+	dp := make([][]int, m+1)
+	for i := 0; i < m+1; i++ {
+		dp[i] = make([]int, n+1)
+	}
+	for i := 1; i < m+1; i++ {
+		for j := 1; j < n+1; j++ {
+			if nums1[i-1] == nums2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[m][n]
+}
+
+func max(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
