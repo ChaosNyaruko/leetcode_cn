1fecfc05cd804fea2384b4d223048fbef729032a
ChaosNyaruko <cabbageparadise@gmail.com>
2021-07-11 00:15:03 +0800

    274.H指数.py

---
+++ 274.H指数.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+        h = 0
+        for c in citations:
+            if c > h:
+                h += 1
+        
+        return h
