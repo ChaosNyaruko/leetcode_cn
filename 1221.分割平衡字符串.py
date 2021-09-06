c97b8cca521e45080310ebbd43124b2bfd5f2508
ChaosNyaruko <cabbageparadise@gmail.com>
2021-09-07 00:13:19 +0800

    1221.分割平衡子字符串.py

---
+++ 1221.分割平衡字符串.py
@@ -0,0 +1,14 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        l = 0
+        res = 0
+        for c in s:
+            if c == 'L':
+                l += 1
+            else:
+                l -= 1
+            
+            if l == 0:
+                res += 1
+        
+        return res
