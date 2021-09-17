babdf30841fd5280fc2c227d13090ddf7992d5c3
ChaosNyaruko <cabbageparadise@gmail.com>
2021-09-17 09:14:20 +0800

    bitmap 有效的数独

--- 36.有效的数独.py
+++ 36.有效的数独.py
@@ -21,7 +21,26 @@ class Solution:
                 rows[i][num] = True
                 cols[j][num] = True
                 boxes[boxIndex][num] = True
-        return True 
-        
+        return True
+
+    def isValidSudoku1(self, board: List[List[str]]) -> bool:
+        rows = [1 << 9] * 9
+        cols = [1 << 9] * 9
+        cells = [1 << 9] * 9
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == '.':
+                    continue
+                x = ord(board[i][j]) - ord('0')   -  1
+                if (rows[i] & (1 << x)) != 0:
+                    return False
+                rows[i] |= (1<<x)
+                if (cols[j] & (1 << x)) != 0:
+                    return False
+                cols[j] |= (1<<x)
+                if (cells[3 * (i // 3) + j//3] & (1 << x)) != 0:
+                    return False
+                cells[3 * (i // 3) + j//3] |= (1<<x)
+        return True
 # @lc code=end
 
